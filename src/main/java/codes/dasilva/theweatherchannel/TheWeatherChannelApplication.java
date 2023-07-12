cbce50320e88c838a22f80b04f2d61df7c587d9b
Gustavo Silva <gas823@cs.bham.ac.uk>
2023-07-12 19:44:27 +0100

    Initial commit

---
+++ src/main/java/codes/dasilva/theweatherchannel/TheWeatherChannelApplication.java
@@ -0,0 +1,13 @@
+package codes.dasilva.theweatherchannel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TheWeatherChannelApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TheWeatherChannelApplication.class, args);
+    }
+
+}
---
+++ src/test/java/codes/dasilva/theweatherchannel/TheWeatherChannelApplicationTests.java
@@ -0,0 +1,13 @@
+package codes.dasilva.theweatherchannel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TheWeatherChannelApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
